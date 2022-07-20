bb495d0073afb8d160c8224bd3d8b81d3dbf7214
Novanda Ahsan <novandaahsan1@gmail.com>
2022-07-20 09:42:48 +0700

    compress image

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 import nunjucks from "vite-plugin-nunjucks";
 import htmlPurge from "vite-plugin-html-purgecss";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default {
   root: path.resolve(__dirname, "src"),
@@ -14,5 +15,35 @@ export default {
     port: 8080,
     hot: true,
   },
-  plugins: [nunjucks(), htmlPurge()],
+  plugins: [
+    nunjucks(),
+    htmlPurge(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
 };
